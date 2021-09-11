ffbde4fd6b78a9c28d6b6d4d0ffb51c299012f53
A.G Safees Mohamed <65711072+safees@users.noreply.github.com>
2021-09-11 11:53:14 +0530

    Initial commit for my prcatice

---
+++ SampleCode.java
@@ -0,0 +1,10 @@
+public class SampleCode {
+	public static void main(String[] args){
+	
+	System.out.println("Hello Safees");
+	System.out.println("Hello nabee");
+	System.out.println("Hello Safees this is for jenkins practice");
+	System.out.println("Hello nabee this is for jenkins practice");
+	
+	}
+}
